9387e567828c48c315591a8670d298056d8050f5
Jonas <ngjingxun1997@gmail.com>
2025-01-22 00:30:23 +0800

    modified html scripts

--- server.js
+++ server.js
@@ -4,26 +4,23 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files
 app.use(express.static(__dirname));
 
-// routes for html
+// Routes for pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/experience.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'experience.html'));
-});
-
-app.get('/projects.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'projects.html'));
+app.get('/resume.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'resume.html'));
 });
 
 app.get('/socials.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'socials.html'));
 });
 
-// start server
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
